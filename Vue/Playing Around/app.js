0f64d1ab3008b3bebc0ed0e5d3c55d5407ceae72
Elliott Thomlison <elliott.thomlison@gmail.com>
2020-09-28 13:59:46 -0700

    Use prettier to reformat the files

--- Vue/Playing Around/app.js
+++ Vue/Playing Around/app.js
@@ -9,45 +9,60 @@ const app = Vue.createApp({
       x: 0,
       y: 0,
       mouseLeaves: "Hello",
-      test: 'You can double click, if you want.',
+      test: "You can double click, if you want.",
       showBooks: true,
       books: [
-        { title: 'The Name of Wind', author: 'Butt Boogerman', img: 'assets/1.jpg', isFav: true},
-        { title: 'Brandon Sanderson', author: 'Booger Buttman', img: 'assets/2.jpg', isFav: true},
-        { title: 'The Final Empire', author: 'Man Manman', img: 'assets/3.jpg', isFav: true},
-      ]
-    }
+        {
+          title: "The Name of Wind",
+          author: "Butt Boogerman",
+          img: "assets/1.jpg",
+          isFav: true,
+        },
+        {
+          title: "Brandon Sanderson",
+          author: "Booger Buttman",
+          img: "assets/2.jpg",
+          isFav: true,
+        },
+        {
+          title: "The Final Empire",
+          author: "Man Manman",
+          img: "assets/3.jpg",
+          isFav: true,
+        },
+      ],
+    };
   },
   methods: {
     toggleFav(book) {
-      console.log('test')
-      book.isFav = !book.isFav
+      console.log("test");
+      book.isFav = !book.isFav;
     },
-    toggleShowBooks( ){
-      this.showBooks = !this.showBooks
+    toggleShowBooks() {
+      this.showBooks = !this.showBooks;
     },
     changeTitle1() {
       // console.log('Test')
-      this.title = 'Words of Radiance'
-      this.candy = 'Booty de Vicky'
-      this.author = 'Shakespeare'
+      this.title = "Words of Radiance";
+      this.candy = "Booty de Vicky";
+      this.author = "Shakespeare";
     },
     toggleShowBooks() {
       this.changeTitle = !this.changeTitle;
     },
     handleEvent() {
-      console.log('test')
-      if (this.test === 'Double Click?') {
-        return this.test = 'Double Clicked'
+      console.log("test");
+      if (this.test === "Double Click?") {
+        return (this.test = "Double Clicked");
       } else {
-        return this.test = 'Double Click?'
+        return (this.test = "Double Click?");
       }
     },
     mouseLeave() {
-      if (this.mouseLeaves === 'Hello') {
-        return this.mouseLeaves = 'Goodbye'
+      if (this.mouseLeaves === "Hello") {
+        return (this.mouseLeaves = "Goodbye");
       } else {
-        return this.mouseLeaves = 'Hello'
+        return (this.mouseLeaves = "Hello");
       }
     },
     handleMouseMove(event) {
@@ -55,12 +70,12 @@ const app = Vue.createApp({
       this.y = event.offsetY;
     },
   },
-  //a way to define data properties that rely on other data 
+  //a way to define data properties that rely on other data
   computed: {
     filteredBooks() {
-      return this.books.filter((book) => book.isFav)
-    }
-  }
-})
+      return this.books.filter((book) => book.isFav);
+    },
+  },
+});
 
-app.mount('#body')
+app.mount("#body");

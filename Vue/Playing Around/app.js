e12a9a641ef7890cdb251de827a1855aed332c41
Elliott Thomlison <elliott.thomlison@gmail.com>
2020-10-03 11:11:59 -0700

    Adjust the printout of the books to include an image

--- Vue/Playing Around/app.js
+++ Vue/Playing Around/app.js
@@ -12,9 +12,9 @@ const app = Vue.createApp({
       test: 'You can double click, if you want.',
       showBooks: true,
       books: [
-        { title: 'Book', author: 'BookA'},
-        { title: 'Book1', author: 'Book1A'},
-        { title: 'Book2', author: 'Book2A'},
+        { title: 'Book', author: 'BookA', img: 'assets/1.jpg'},
+        { title: 'Book1', author: 'Book1A', img: 'assets/2.jpg'},
+        { title: 'Book2', author: 'Book2A', img: 'assets/3.jpg'},
       ]
     }
   },

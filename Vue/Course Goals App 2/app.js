a9015d8536ab487737330037ade491324a6df209
elliottthomlison <71367849+elliottthomlison@users.noreply.github.com>
2020-11-17 12:38:46 -0800

    Upload the functionality of adding to the goals array

--- Vue/Course Goals App 2/app.js
+++ Vue/Course Goals App 2/app.js
@@ -1,7 +1,15 @@
 const app = Vue.createApp({
   data() {
-    return { goals: [] };
+    return { 
+      enterGoalValue: '',
+      goals: [] 
+    };
   },
+  methods: {
+    addGoal() {
+      this.goals.push(this.enterGoalValue);
+    }
+  }
 });
 
 app.mount('#user-goals');

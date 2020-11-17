28fa4903f9e29ff2b9674993e0a65a8823cfa59f
elliottthomlison <71367849+elliottthomlison@users.noreply.github.com>
2020-11-17 12:39:02 -0800

    Utilize Prettier

--- Vue/Course Goals App 2/app.js
+++ Vue/Course Goals App 2/app.js
@@ -1,15 +1,15 @@
 const app = Vue.createApp({
   data() {
-    return { 
-      enterGoalValue: '',
-      goals: [] 
+    return {
+      enterGoalValue: "",
+      goals: [],
     };
   },
   methods: {
     addGoal() {
       this.goals.push(this.enterGoalValue);
-    }
-  }
+    },
+  },
 });
 
-app.mount('#user-goals');
+app.mount("#user-goals");

7aabfc51d5de70284073007afe17c26c71eb25ab
elliottthomlison <71367849+elliottthomlison@users.noreply.github.com>
2020-11-25 13:11:29 -0800

    Update the functionality to include feedback when the counter meets requirements

--- Vue/Reactivity App/app.js
+++ Vue/Reactivity App/app.js
@@ -14,13 +14,23 @@ const app = Vue.createApp({
   },
   computed: {
     counterCheck() {
-      if (this.counter === 37){
-        return 'Perfect amount!'
-      } else if (this.counter > 37){
-        return 'Too much!'
-      } 
-    }
+      if (this.counter === 37) {
+        return "Perfect amount!";
+      } else if (this.counter > 37) {
+        return "Too much!";
+      }
+    },
+  },
+  watch: {
+    counter(value) {
+      if (value > 0) {
+        const that = this;
+        setTimeout(function () {
+          that.counter = 0;
+        }, 5000);
+      }
+    },
   },
 });
 
-app.mount('#assignment');
+app.mount("#assignment");

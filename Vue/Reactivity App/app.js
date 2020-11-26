a2e0e3b8b1628d2a96968bb004d4453c4201b8b0
elliottthomlison <71367849+elliottthomlison@users.noreply.github.com>
2020-11-26 12:52:34 -0800

    Update the functionality to include a counter

--- Vue/Reactivity App/app.js
+++ Vue/Reactivity App/app.js
@@ -1,5 +1,16 @@
 const app = Vue.createApp({
   data() {
+    return {
+      counter: 0,
+    };
+  },
+  methods: {
+    add(num) {
+      this.counter = this.counter + num;
+    },
+    subtract(num) {
+      this.counter = this.counter - num;
+    },
   }
 });
 

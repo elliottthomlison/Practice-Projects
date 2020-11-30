87949b54ca317915cf37f435c08b8d2ea139e0f7
elliottthomlison <71367849+elliottthomlison@users.noreply.github.com>
2020-11-30 14:05:17 -0800

    Adjust the display properties of the page

--- Vue/Vue Events App/app.js
+++ Vue/Vue Events App/app.js
@@ -3,6 +3,7 @@ const app = Vue.createApp({
     return {
       counter: 0,
       name: "",
+      confirmedName: ""
     };
   },
   methods: {
@@ -11,6 +12,9 @@ const app = Vue.createApp({
     // },
     setName(event, lastName) {
       this.name = event.target.value + " " + lastName;
+    },
+    confirmInput() {
+      this.confirmedName = this.name;
     },
     submitForm() {
       alert("Submitted!");

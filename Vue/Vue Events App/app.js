36acea2683ad15e6bb3fca5c88b565d54505778a
elliottthomlison <71367849+elliottthomlison@users.noreply.github.com>
2020-12-01 14:01:33 -0800

    Reformat with prettier

--- Vue/Vue Events App/app.js
+++ Vue/Vue Events App/app.js
@@ -2,28 +2,28 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
-      name: '',
+      name: "",
     };
   },
-    methods: {
+  methods: {
     //   setName(event) {
     //     this.name = event.target.value;
     // },
-      setName(event, lastName) {
-        this.name = event.target.value + ' ' + lastName;
-      },
-      submitForm() {
-        alert('Submitted!');
-      },
-      add(num) {
-        // this.counter ++
-        this.counter = this.counter + num;
+    setName(event, lastName) {
+      this.name = event.target.value + " " + lastName;
     },
-      subtract(num) {
-        // this.counter --
-        this.counter = this.counter - num;
-    }
-  }
+    submitForm() {
+      alert("Submitted!");
+    },
+    add(num) {
+      // this.counter ++
+      this.counter = this.counter + num;
+    },
+    subtract(num) {
+      // this.counter --
+      this.counter = this.counter - num;
+    },
+  },
 });
 
-app.mount('#events');
+app.mount("#events");

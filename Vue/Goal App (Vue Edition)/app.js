f3ebeed1017b6282c71a7485124822ff1c97e083
elliottthomlison <71367849+elliottthomlison@users.noreply.github.com>
2021-06-30 12:59:44 -0700

    Add Vue code to the app.js file as a first step

--- Vue/Goal App (Vue Edition)/app.js
+++ Vue/Goal App (Vue Edition)/app.js
@@ -1,13 +1,7 @@
-const buttonEl = document.querySelector('button');
-const inputEl = document.querySelector('input');
-const listEl = document.querySelector('ul');
+Vue.createApp({
+  data() {
+    return {
 
-function addGoal() {
-  const enteredValue = inputEl.value;
-  const listItemEl = document.createElement('li');
-  listItemEl.textContent = enteredValue;
-  listEl.appendChild(listItemEl);
-  inputEl.value = '';
-}
-
-buttonEl.addEventListener('click', addGoal);
+    };
+  }
+})

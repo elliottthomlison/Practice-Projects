4b59dc4c1c06d0cd57dab141e4263fbb1d4d1a23
elliottthomlison <71367849+elliottthomlison@users.noreply.github.com>
2021-07-04 12:04:51 -0700

    Adjust the functions that control the random number and future age properties

--- Vue/Personal Questions App/app.js
+++ Vue/Personal Questions App/app.js
@@ -3,13 +3,17 @@ const app = Vue.createApp({
     return {
       yourName: 'Elliott Thomlison',
       yourAge: 28,
-      yourFutureAge: 33,
-      yourNumber: Math.floor((Math.random() * 100) + 1),
+      // yourNumber: Math.floor((Math.random() * 100) + 1),
       vueLink: 'https://images.unsplash.com/photo-1606388283827-e9db3daa18d7?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1048&q=80',
     };
   },
   methods: {
-    
+    calculateAge() {
+      return this.yourAge + 5;
+    },
+    yourNumber() {
+      return Math.floor((Math.random() * 100) + 1)
+    }
   }
 });
 

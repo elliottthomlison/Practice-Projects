d8307dcdfeea5d88708073c5dbf801e99dbdffed
Elliott Thomlison <elliott.thomlison@gmail.com>
2021-07-12 14:13:03 -0700

    Add the functionality of the mini eggs increasing upon click

--- Vue/Playing Around/app.js
+++ Vue/Playing Around/app.js
@@ -3,8 +3,9 @@ const app = Vue.createApp({
     return {
       title: "A Test Page",
       author: "Elliott Thomlison",
-      candy: "Mini Eggs"
-    }
+      candy: "Mini Eggs",
+      amount: 0,
+    } 
   }
 })
 

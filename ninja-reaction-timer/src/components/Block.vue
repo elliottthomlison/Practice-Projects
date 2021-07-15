b02a460e3cc04c288a61dacabbcdc0507d914386
Elliott Thomlison <elliott.thomlison@gmail.com>
2021-07-15 16:29:28 -0700

    Utilize prettier to reformat the code

--- ninja-reaction-timer/src/components/Block.vue
+++ ninja-reaction-timer/src/components/Block.vue
@@ -1,55 +1,56 @@
 <template>
   <div class="block" v-if="showBlock" @click="stopTimer">
-      Click Me
+    Click Me
   </div>
 </template>
 
 <script>
 export default {
-    props: ['delay'],
-    data() {
-        return {
-            showBlock: false,
-            timer: null,
-            reactionTime: 0,
-        }
+  props: ["delay"],
+  data() {
+    return {
+      showBlock: false,
+      timer: null,
+      reactionTime: 0,
+    };
+  },
+  mounted() {
+    console.log("Mounted");
+    setTimeout(() => {
+      this.showBlock = true;
+      this.startTimer();
+      // console.log('After Amount:', this.delay)
+    }, this.delay);
+  },
+  // updated() {
+  //     console.log('Component Updated')
+  // },
+  // unmounted() {
+  //     console.log('Component Unmounted')
+  // }
+  methods: {
+    startTimer() {
+      this.timer = setInterval(() => {
+        this.reactionTime += 10;
+      }, 10);
     },
-    mounted() {
-        console.log('Mounted')
-        setTimeout(() => {
-            this.showBlock = true
-            this.startTimer()
-            // console.log('After Amount:', this.delay)
-        }, this.delay)
+    stopTimer() {
+      clearInterval(this.timer);
+      // console.log(this.reactionTime)
+      this.$emit("finish", this.reactionTime);
     },
-    // updated() {
-    //     console.log('Component Updated')
-    // },
-    // unmounted() {
-    //     console.log('Component Unmounted')
-    // }
-    methods: {
-        startTimer() {
-            this.timer = setInterval(() => {
-                this.reactionTime += 10
-            }, 10)
-        },
-        stopTimer() {
-            clearInterval(this.timer)
-            console.log(this.reactionTime)
-        },
-    }
-}
+  },
+};
 </script>
 
 <style>
-    .block {
-        width: 400px;
-        border-radius: 20px;
-        background: rgb(59, 177, 108);
-        color: white;
-        text-align: center;
-        padding: 100px 0;
-        margin: 40px auto;
-    }
+.block {
+  width: 400px;
+  border-radius: 20px;
+  background: rgb(59, 177, 108);
+  color: white;
+  text-align: center;
+  padding: 100px 0;
+  margin: 40px auto;
+}
 </style>

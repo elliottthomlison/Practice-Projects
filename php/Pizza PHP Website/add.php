666c29259920ea61316a4c6fc81739fbc86a97e8
elliottthomlison <elliott.thomlison@gmail.com>
2021-07-21 08:42:14 -0700

    Create a form that will hold values for a pizza

--- php/Pizza PHP Website/add.php
+++ php/Pizza PHP Website/add.php
@@ -7,6 +7,18 @@
 	
 	<?php include('templates/header.php'); ?>
 
+    <section class="container grey-text">
+        <h4 class="center">Add a Pizza</h4>
+        <form class="white" action="" method="">
+            <label>Your Email:</label>
+            <input type="text" name="email"></input>
+            <label>Pizza Title:</label>
+            <input type="text" name="title"></input>
+            <label>Ingredients:</label>
+            <input type="text" name="ingredients"></input>
+        </form>
+    </section>
+
 	<?php include('templates/footer.php'); ?>
 
-</html>
+</html>a

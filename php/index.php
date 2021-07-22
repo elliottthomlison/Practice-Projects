fa07fcc82ad0e68309ceb5a0862d8a2e0adedcda
elliottthomlison <elliott.thomlison@gmail.com>
2021-07-22 14:55:00 -0700

    Re-add the files that were accidentally deleted

---
+++ php/index.php
@@ -0,0 +1,31 @@
+<?php
+
+$name = "Yoshi";
+$age = 30;
+
+$name = "Mario";
+
+echo $name
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My First PHP File</title>
+</head>
+
+<body>
+    <h1>
+        User Profile Page
+    </h1>
+    
+    <div><?php echo $name ?></div>
+    <div><?php echo $age ?></div>
+</body>
+
+</html>

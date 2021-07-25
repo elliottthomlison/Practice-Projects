7c5fc81d465f74f54f67d2cf0dc31dc46e71c994
elliottthomlison <elliott.thomlison@gmail.com>
2021-07-25 11:54:01 -0700

    Refactor with Prettier and set conditional display settings to the winner container

--- Vue/The Monster Slayer Game/app.js
+++ Vue/The Monster Slayer Game/app.js
@@ -14,6 +14,7 @@ const app = Vue.createApp({
       currentRound: 0,
       specialAttack: 0,
       healValue: 0,
+      winner: null,
     };
   },
   methods: {
@@ -22,29 +23,29 @@ const app = Vue.createApp({
       const attackValue = getRandomValue(5, 10);
       this.monsterHealth = this.monsterHealth - attackValue;
       this.attackPlayer();
-      this.currentRound++
+      this.currentRound++;
     },
     attackPlayer() {
       const attackValue = getRandomValue(10, 15);
       this.playerHealth = this.playerHealth - attackValue;
     },
     specialAttackMonster() {
-        const attackValue = getRandomValue(10, 25);
-        this.monsterHealth = this.monsterHealth - attackValue
-        this.attackPlayer() ;
-        this.currentRound++
+      const attackValue = getRandomValue(10, 25);
+      this.monsterHealth = this.monsterHealth - attackValue;
+      this.attackPlayer();
+      this.currentRound++;
     },
     healPlayer() {
-        this.currentRound++
-        const healValue = getRandomValue(5, 20);
-        if (this.playerHealth + healValue > 100) {
-            this.playerHealth = 100;
-        } else {
-            this.playerHealth = this.playerHealth + healValue
-            currentRound++
-        }
-        this.attackPlayer();
-    }
+      this.currentRound++;
+      const healValue = getRandomValue(5, 20);
+      if (this.playerHealth + healValue > 100) {
+        this.playerHealth = 100;
+      } else {
+        this.playerHealth = this.playerHealth + healValue;
+        currentRound++;
+      }
+      this.attackPlayer();
+    },
   },
   computed: {
     monsterBarStyles() {
@@ -54,14 +55,34 @@ const app = Vue.createApp({
       return { width: this.playerHealth + "%" };
     },
     monsterSpecialBarStyles() {
-        return { width: this.playerHealth + "%"}
+      return { width: this.playerHealth + "%" };
     },
     useSpecialAttack() {
-        return this.currentRound % 3 !== 0
+      return this.currentRound % 3 !== 0;
     },
-    healthCheck () {
-        return this.currentRound % 2 !== 0
-    }
+    healthCheck() {
+      return this.currentRound % 2 !== 0;
+    },
+  },
+  watch: {
+    playerHealth(value) {
+      if (value <= -0 && this.monsterHealth <= 0) {
+        //a draw
+        this.winner = "draw";
+      } else if (value <= 0) {
+        //player lost
+        this.winner = "monster";
+      }
+    },
+    monsterHealth(value) {
+      if (value <= 0 && this.playerHealth <= 0) {
+        //a draw
+        this.winner = "draw";
+      } else if (value <= 0) {
+        //monster lost
+        this.winner = "player";
+      }
+    },
   },
 });
 

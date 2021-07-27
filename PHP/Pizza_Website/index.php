9c6a5ac5843e748acba3001915d457003a147665
elliottthomlison <elliott.thomlison@gmail.com>
2021-07-27 15:51:51 -0700

    Use PHPMyAdmin and connect to the database of pizzas, print out the results

--- PHP/Pizza_Website/index.php
+++ PHP/Pizza_Website/index.php
@@ -1,5 +1,22 @@
 <?php 
+	//connect to db
+	$conn = mysqli_connect('localhost', 'elliott', 'weedeater', 'ninja_pizza');
 
+	//check connection
+	if(!$conn) {
+		echo'Connection Error: ' . mysqli_connect_error();
+	}
+
+	//write query for all pizzas
+	$sql = 'SELECT title, ingredients, id FROM pizzas';
+
+	//make query & get result
+	$result = mysqli_query($conn, $sql);
+
+	//fetch the resulting rows as an array
+	$pizzas = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+	print_r($pizzas);
 ?>
 
 <!DOCTYPE html>

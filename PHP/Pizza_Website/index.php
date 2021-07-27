393ffdd1a21c44dc51d255f934f9a9a79ca0f38e
elliottthomlison <elliott.thomlison@gmail.com>
2021-07-27 16:32:31 -0700

    Connect the DB and PHP so that they work together

--- PHP/Pizza_Website/index.php
+++ PHP/Pizza_Website/index.php
@@ -16,6 +16,12 @@
 	//fetch the resulting rows as an array
 	$pizzas = mysqli_fetch_all($result, MYSQLI_ASSOC);
 
+	//free result from memory
+	mysqli_free_result($result);
+
+	//close connection
+	mysqli_close($conn);
+
 	print_r($pizzas);
 ?>
 
@@ -23,6 +29,30 @@
 <html>
 	
 	<?php include('templates/header.php'); ?>
+
+	<h4 class="center grey-text">Pizzas!</h4>
+
+	<div class="container">
+		<div class="row">
+
+			<?php foreach($pizzas as $pizza){ ?>
+				
+				<div class="col s6 md3">
+					<div class="card z-depth-0">
+						<div class="card-content center">
+							<h6><?php echo htmlspecialchars($pizza['title']); ?></h6>
+							<div><?php echo htmlspecialchars($pizza['ingredients']); ?></div>
+						</div>
+						<div class="card-action right-align">
+							<a href="#" class="brand-text">more info</a>
+						</div>
+					</div>
+				</div>
+			
+			<?php } ?>
+
+		</div>
+	</div>
 
 	<?php include('templates/footer.php'); ?>
 

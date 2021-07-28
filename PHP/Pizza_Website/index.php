cd4566f5750657ccb805f999007756a53acde8dc
elliottthomlison <elliott.thomlison@gmail.com>
2021-07-28 09:41:46 -0700

    Create routes for the website with the appropriate messages

--- PHP/Pizza_Website/index.php
+++ PHP/Pizza_Website/index.php
@@ -1,24 +1,23 @@
 <?php 
-	include('/config/db_connect.php');
 
-	//write query for all pizzas
+	include('config/db_connect.php');
+
+	// write query for all pizzas
 	$sql = 'SELECT title, ingredients, id FROM pizzas ORDER BY created_at';
 
-	//make query & get result
+	// get the result set (set of rows)
 	$result = mysqli_query($conn, $sql);
 
-	//fetch the resulting rows as an array
+	// fetch the resulting rows as an array
 	$pizzas = mysqli_fetch_all($result, MYSQLI_ASSOC);
 
-	//free result from memory
+	// free the $result from memory (good practise)
 	mysqli_free_result($result);
 
-	//close connection
+	// close connection
 	mysqli_close($conn);
 
-	// print_r(explode(',', $pizzas[0]['ingredients']));
 
-	
 ?>
 
 <!DOCTYPE html>
@@ -32,30 +31,24 @@
 		<div class="row">
 
 			<?php foreach($pizzas as $pizza): ?>
-				
-				<div class="col s6 md3">
+
+				<div class="col s6 m4">
 					<div class="card z-depth-0">
 						<div class="card-content center">
 							<h6><?php echo htmlspecialchars($pizza['title']); ?></h6>
-							<ul>
+							<ul class="grey-text">
 								<?php foreach(explode(',', $pizza['ingredients']) as $ing): ?>
 									<li><?php echo htmlspecialchars($ing); ?></li>
 								<?php endforeach; ?>
 							</ul>
 						</div>
 						<div class="card-action right-align">
-							<a href="#" class="brand-text">more info</a>
+							<a class="brand-text" href="details.php?id=<?php echo $pizza['id'] ?>">more info</a>
 						</div>
 					</div>
 				</div>
-			
-			<?php endforeach; ?>
 
-			<?php if(count($pizzas) >= 3): ?>
-				<p>There are 2 or more pizzas</p>
-			<?php else: ?>
-				<p>There are less than 3 pizzas</p>
-			<?php endif; ?>
+			<?php endforeach; ?>
 
 		</div>
 	</div>

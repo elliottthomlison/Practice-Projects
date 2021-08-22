b5547ab8bbaca25d539fa0dd0a16146e8ce546e1
elliottthomlison <elliott.thomlison@gmail.com>
2021-08-22 14:54:30 -0700

    Create new Python projects

---
+++ Python/venv/app.py
@@ -0,0 +1,9 @@
+# age = 20
+# price = 19.95
+# name = "John Smith"
+
+# print('Hello', name, "I see that you are", age, "years old.")
+
+name = input("What is your name? ")
+print("Hello " + name)
+

795389e36a36be4e945568195e2fad320b2272cb
elliottthomlison <elliott.thomlison@gmail.com>
2021-08-24 15:21:59 -0700

    Adjust the Python code to create a dictionary

--- Python/game/game.py
+++ Python/game/game.py
@@ -1,6 +1,11 @@
-player_name = 'Manuel'
-player_attack = 10
-player_heal = 16
-health = 100
+# player_name = 'Manuel'
+# player_attack = 10
+# player_heal = 16
+# health = 100
 
-print(player_attack)
+# player = ['Manuel', 10, 16, 100]
+# print(player[0])
+
+player = {'name': 'Manuel', 'attack': 10, 'heal': 16, 'health': 100}
+monster = {'name': 'Max', 'attack': 12, 'health': 100}
+print(type(player))

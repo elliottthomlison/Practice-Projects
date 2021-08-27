088ddc2ffa189af56b0ae60129f7eaa1da0ccd08
elliottthomlison <elliott.thomlison@gmail.com>
2021-08-26 19:00:18 -0700

    Update the script of the react project

--- React/counter/src/Main.js
+++ React/counter/src/Main.js
@@ -1,21 +1,43 @@
 import React, { Component } from "react";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './Main.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./Main.css";
 
 class Main extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      number: 0,
+    };
   }
+  increment = () => {
+    this.setState({ number: this.state.number + 1 });
+  };
+
+  decrement = () => {
+    this.setState({ number: this.state.number - 1 });
+  };
+
+  home = () => {
+      this.setState({ number: 0 })
+  }
+
   render() {
     return (
-      <div className="container">
-        <h1>React Counter</h1>
-        <h1>0</h1>
-        <div>
-            <button className="btn btn-success">Increase</button>
-            <button className="btn btn-danger">Decrease</button>
-            <button className="btn btn-primary">Return to 0</button>
+      <div className="body">
+        <div className="container">
+          <h1>React Counter</h1>
+          <h1>{this.state.number}</h1>
+          <div>
+            <button className="btn btn-success" onClick={this.increment}>
+              Increase
+            </button>
+            <button className="btn btn-danger" onClick={this.decrement}>
+              Decrease
+            </button>
+            <button className="btn btn-primary" onClick={this.home}>
+              Return to 0
+            </button>
+          </div>
         </div>
       </div>
     );

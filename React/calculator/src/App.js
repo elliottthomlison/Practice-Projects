a4bd92bc5e75b0c1fae41f489192b5daf8837cf4
elliottthomlison <elliott.thomlison@gmail.com>
2021-08-28 13:33:12 -0700

    Update the react calculator project with new code

--- React/calculator/src/App.js
+++ React/calculator/src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-6 text-center">
+          <table class="table">
+                    <tr>
+                        <td colspan="3"><input type="text" id="textarea"/></td>
+                        <td><input type="button" value="c" onclick="clr()"/></td>
+                    </tr>
+                    <tr>
+                        <td><input type="button" value="1" onclick="display('1')"/></td>
+                        <td><input type="button" value="2" onclick="display('2')"/></td>
+                        <td><input type="button" value="3" onclick="display('3')"/></td>
+                        <td><input type="button" value="/" onclick="display('/')"/></td>
+                    </tr>
+                    <tr>
+                        <td><input type="button" value="4" onclick="display('4')"/></td>
+                        <td><input type="button" value="5" onclick="display('5')"/></td>
+                        <td><input type="button" value="6" onclick="display('6')"/></td>
+                        <td><input type="button" value="-" onclick="display('-')"/></td>
+                    </tr>
+                    <tr>
+                        <td><input type="button" value="7" onclick="display('7')"/></td>
+                        <td><input type="button" value="8" onclick="display('8')"/></td>
+                        <td><input type="button" value="9" onclick="display('9')"/></td>
+                        <td><input type="button" value="+" onclick="display('+')"/></td>
+                    </tr>
+                    <tr>
+                        <td><input type="button" value="." onclick="display('.')"/></td>
+                        <td><input type="button" value="0" onclick="display('0')"/></td>
+                        <td><input type="button" value="=" onclick="calculate()"/></td>
+                        <td><input type="button" value="*" onclick="display('*')"/></td>
+                    </tr>
+                </table>
+            </div>
+        </div>
     </div>
   );
 }

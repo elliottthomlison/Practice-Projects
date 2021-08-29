dfc6360273b17a458cd34f9e47dfaa99544f3911
elliottthomlison <elliott.thomlison@gmail.com>
2021-08-29 14:08:01 -0700

    Add styling and synatical changes to the project

--- React/calculator/src/Main.js
+++ React/calculator/src/Main.js
@@ -6,36 +6,36 @@ function Main() {
     return (
         <div className="container">
             <div className="row justify-content-center">
-                <div className="col-md-6 text-center">
+                <div className="col-md-5 shadow-lg p-3 mb-5 bg-white rounded">
                 <h1>React Calculator</h1>
                     <table className="table">
                         <tr>
-                            <td colspan="3"><input type="text" id="textarea"/></td>
-                            <td><input type="button" value="C" onclick="clr()"/></td>
+                            <td colSpan="3"><input type="text"/></td>
+                            <td><button>C</button></td>
                         </tr>
                         <tr>
-                            <td><input type="button" value="1" onclick="display('1')"/></td>
-                            <td><input type="button" value="2" onclick="display('2')"/></td>
-                            <td><input type="button" value="3" onclick="display('3')"/></td>
-                            <td><input type="button" value="/" onclick="display('/')"/></td>
+                            <td><button>1</button></td>
+                            <td><button>2</button></td>
+                            <td><button>3</button></td>
+                            <td><button>/</button></td>
                         </tr>
                         <tr>
-                            <td><input type="button" value="4" onclick="display('4')"/></td>
-                            <td><input type="button" value="5" onclick="display('5')"/></td>
-                            <td><input type="button" value="6" onclick="display('6')"/></td>
-                            <td><input type="button" value="-" onclick="display('-')"/></td>
+                            <td><button>4</button></td>
+                            <td><button>5</button></td>
+                            <td><button>6</button></td>
+                            <td><button>-</button></td>
                         </tr>
                         <tr>
-                            <td><input type="button" value="7" onclick="display('7')"/></td>
-                            <td><input type="button" value="8" onclick="display('8')"/></td>
-                            <td><input type="button" value="9" onclick="display('9')"/></td>
-                            <td><input type="button" value="+" onclick="display('+')"/></td>
+                            <td><button>7</button></td>
+                            <td><button>8</button></td>
+                            <td><button>9</button></td>
+                            <td><button>+</button></td>
                         </tr>
                         <tr>
-                            <td><input type="button" value="." onclick="display('.')"/></td>
-                            <td><input type="button" value="0" onclick="display('0')"/></td>
-                            <td><input type="button" value="=" onclick="calculate()"/></td>
-                            <td><input type="button" value="*" onclick="display('*')"/></td>
+                            <td><button>.</button></td>
+                            <td><button>0</button></td>
+                            <td><button>=</button></td>
+                            <td><button>*</button></td>
                         </tr>
                     </table>
                 </div>

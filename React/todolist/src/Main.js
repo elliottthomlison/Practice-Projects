eaade8a7625739e91edb3a9e054f9b5c72cce9e9
elliottthomlison <elliott.thomlison@gmail.com>
2021-09-09 20:53:27 -0700

    Update dependencies and the logic of the todo list project

--- React/todolist/src/Main.js
+++ React/todolist/src/Main.js
@@ -3,12 +3,30 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import './Main.css'
 
 function Main() {
-    return(
+    const [taskName, setTaskName] = useState('');
+    const [taskList, setTaskList] = useState([]);
+
+    function addTask() {
+        setTaskList([...taskList, taskName])
+    }
+    
+    const taskListContent = taskList.map((task) => {
+        return (
+            <div>
+                <p>{task}</p>
+                <button>Delete</button>
+            </div>
+        )
+    })
+
+    return (
         <div className="row justify-content-center">
             <div className="col-md-5">
                 <h1>React To Do List</h1>
-                <input className="form-control" id="taskName" type="text" placeholder="Enter Task" />
-                <button onclick="" class="btn btn-primary">Add</button>
+                <input className="form-control" id="taskName" type="text" placeholder="Enter Task" value={taskName} onChange={(e) => {setTaskName(e.target.value)}}/>
+                <button onclick="" className="btn btn-primary" onClick={addTask}>Add</button>
+
+                {taskListContent}
             </div>
         </div>
     )

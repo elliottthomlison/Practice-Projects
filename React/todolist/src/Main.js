00f8dc822a99ac7c646c7168cf3fa38079d17d2c
elliottthomlison <elliott.thomlison@gmail.com>
2021-09-11 21:15:06 -0700

    Update the logic of the assignment and upload completed project

--- React/todolist/src/Main.js
+++ React/todolist/src/Main.js
@@ -8,13 +8,20 @@ function Main() {
 
     function addTask() {
         setTaskList([...taskList, taskName])
+        this.taskName.value = ''
     }
     
-    const taskListContent = taskList.map((task) => {
+    function deleteTask(index) {
+        const array = [...taskList]
+        array.splice(index, 1)
+        setTaskList(array)
+    }
+
+    const taskListContent = taskList.map((task, index) => {
         return (
             <div id="taskListContentItems">
                 <p>{task}</p>
-                <i class="far fa-trash-alt"></i>
+                <i className="far fa-trash-alt" onClick={()=>deleteTask(index)}></i>
             </div>
         )
     })
@@ -24,7 +31,7 @@ function Main() {
             <div className="col-md-5">
                 <h1>React To Do List</h1>
                 <input className="form-control" id="taskName" type="text" placeholder="Enter Task" value={taskName} onChange={(e) => {setTaskName(e.target.value)}}/>
-                <button onclick="" className="btn btn-primary" onClick={addTask}>Add</button>
+                <button className="btn btn-primary" onClick={addTask}>Add</button>
 
                 {taskListContent}
             </div>

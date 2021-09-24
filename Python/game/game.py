901b93f10f6707069ca89e4cc2998fb2064781a8
elliottthomlison <elliott.thomlison@gmail.com>
2021-09-24 07:51:25 -0700

    Refactored code and adjusted for typos

--- Python/game/game.py
+++ Python/game/game.py
@@ -1,45 +1,45 @@
-# player_name = 'Manuel'
+# player_name = "Manuel"
 # player_attack = 10
 # player_heal = 16
 # health = 100
 
-# player = ['Manuel', 10, 16, 100]
+# player = ["Manuel", 10, 16, 100]
 # print(player[0])
 
 # print(type(player))
 
-player = {'name': 'Manuel', 'attack': 10, 'heal': 16, 'health': 100}
-monster = {'name': 'Max', 'attack': 12, 'health': 100}
+player = {"name": "Manuel", "attack": 10, "heal": 16, "health": 100}
+monster = {"name": "Max", "attack": 12, "health": 100}
 game_running = True
 
 while game_running == True:
 
-    print('Please select action')
-    print('1) Attack')
-    print('2) Heal')
+    print("Please select action")
+    print("1) Attack")
+    print("2) Heal")
 
     player_choice = input()
     # print(type(player_choice))
 
-    if player_choice == '1':
-        print('Player Attacks')
-        monster['health'] = monster['health'] - player['attack']
-        print("Monster's Health:")
-        print(monster['health'])
+    if player_choice == "1":
+        print("Player Attacks")
+        monster["health"] = monster["health"] - player["attack"]
+        print("Monster"s Health:")
+        print(monster["health"])
         
-        print('Monster Attacks')
-        player['health'] = player['health'] - monster['attack']
-        print("Player's Health:")
-        print(player['health'])
+        print("Monster Attacks")
+        player["health"] = player["health"] - monster["attack"]
+        print("Player"s Health:")
+        print(player["health"])
 
-    elif player_choice == '2':
-        print('Heal')
-        player['health'] = player['health'] + player['heal']
-        print(player['health'])
+    elif player_choice == "2":
+        print("Heal")
+        player["health"] = player["health"] + player["heal"]
+        print(player["health"])
 
     else :
-        print('Invalid input')
+        print("Invalid input")
     
-    if player['health'] <= 0 or monster['health'] <= 0:
+    if player["health"] <= 0 or monster["health"] <= 0:
         game_running = False
         

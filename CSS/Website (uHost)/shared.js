5dfd4fa29277e13e03c8d48dfe6b64d080566562
elliottthomlison <elliott.thomlison@gmail.com>
2021-09-27 08:50:24 -0700

    Refactored code and adjusted for typos

--- CSS/Website (uHost)/shared.js
+++ CSS/Website (uHost)/shared.js
@@ -5,21 +5,21 @@ var selectPlanButtons = document.querySelectorAll(".plan button");
 var toggleButton = document.querySelector(".toggle-button");
 var mobileNav = document.querySelector(".mobile-nav");
 
-// console.dir(backdrop.style['background-image']);
+// console.dir(backdrop.style["background-image"]);
 
 // console.dir(backdrop);
 for (var i = 0; i < selectPlanButtons.length; i++) {
   selectPlanButtons[i].addEventListener("click", function() {
     // modal.style.display = "block";
     // backdrop.style.display = "block";
-    // modal.className = 'open'; // This will actually overwrite the complete class list
+    // modal.className = "open"; // This will actually overwrite the complete class list
     modal.classList.add("open");
     backdrop.classList.add("open");
   });
 }
 
 backdrop.addEventListener("click", function() {
-  // mobileNav.style.display = 'none';
+  // mobileNav.style.display = "none";
   mobileNav.classList.remove("open");
   closeModal();
 });
@@ -38,8 +38,8 @@ function closeModal() {
 }
 
 toggleButton.addEventListener("click", function() {
-  // mobileNav.style.display = 'block';
-  // backdrop.style.display = 'block';
+  // mobileNav.style.display = "block";
+  // backdrop.style.display = "block";
   mobileNav.classList.add("open");
   backdrop.classList.add("open");
 });

36cebccd877542d7966cf66db736ce16a2fcaef9
elliottthomlison <elliott.thomlison@gmail.com>
2021-10-07 19:34:22 -0700

    Add styling and functional changes to the code

--- React/Idea Bag/two-cities/src/App.js
+++ React/Idea Bag/two-cities/src/App.js
@@ -1,30 +1,14 @@
 import React from 'react';
 import AddCity from './components/Cities/AddCity';
-// import Map from './components/Map/Map'; 
-import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
+
 
 function App() {
   return (
     <div>
       <AddCity />
-      {/* <Map /> */}
-      <Map google={this.props.google} zoom={14}>
- 
-        <Marker onClick={this.onMarkerClick}
-                name={'Current location'} />
- 
-        <InfoWindow onClose={this.onInfoWindowClose}>
-            {/* <div>
-              <h1>{this.state.selectedPlace.name}</h1>
-            </div> */}
-        </InfoWindow>
-      </Map>
     </div>
   );
 }
 
-// export default App;
+export default App;
 
-export default GoogleApiWrapper({
-  apiKey: ("GD2Ucfv_6RaK81f2ZafHWM4fnpdg")
-})()
--- React/Idea Bag/two-cities/src/components/Cities/AddCity.js
+++ React/Idea Bag/two-cities/src/components/Cities/AddCity.js
@@ -1,23 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from '../Button/Button';
 import Card from '../UI/Card';
 import classes from './AddCity.module.css'
 
 const AddCity = (props) => {
+  const [enteredFromCity, setEnteredFromCity] = useState('')
+  const [enteredToCity, setEnteredToCity] = useState('')
+
   const addCityHandler = (event) => {
     event.preventDefault();
     
+    if (enteredFromCity.trim().length === 0 || enteredToCity.trim().length === 0) {
+      return;
+   } 
+      console.log(enteredFromCity, enteredToCity)
+      setEnteredFromCity('');
+      setEnteredToCity('');
   }
 
+  const fromCityChangeHandler = (event) => {
+    setEnteredFromCity('');
+  }
+
+  const toCityChangeHandler = (event) => {
+    setEnteredToCity('');
+  }
 
   return (
     // import the card after
     <Card className={classes.input}>
       <form onSubmit={addCityHandler}>
-        <label htmlFor="fromCity">From:</label>
-        <input id="fromcity" type="text" />
-        <label htmlFor="toCity">To:</label>
-        <input id="toCity" type="text" />
+        <label htmlFor="fromCity">From</label>
+        <input id="fromCity" type="text" value={enteredFromCity} onChange={fromCityChangeHandler}/>
+        <label htmlFor="toCity">To</label>
+        <input id="toCity" type="text"  value={enteredToCity} onChange={toCityChangeHandler}/>
         <Button />
       </form>
     </Card>

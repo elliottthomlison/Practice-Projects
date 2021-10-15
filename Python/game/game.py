527b2e2a626557d9c3f8d3a26c7e532c5163d9bd
elliottthomlison <elliott.thomlison@gmail.com>
2021-10-15 12:40:16 -0700

    Upload new folders for React practice

--- Python/game/game.py
+++ Python/game/game.py
@@ -10,25 +10,36 @@
 
 player = {'name': 'Manuel', 'attack': 10, 'heal': 16, 'health': 100}
 monster = {'name': 'Max', 'attack': 12, 'health': 100}
-print('Please select action')
-print('1) Attack')
-print('2) Heal')
+game_running = True
 
-player_choice = input()
-# print(type(player_choice))
+while game_running == True:
 
-if player_choice == '1':
-    print('Player Attacks')
-    monster['health'] = monster['health'] - player['attack']
-    print("Monster's Health:")
-    print(monster['health'])
+    print('Please select action')
+    print('1) Attack')
+    print('2) Heal')
+
+    player_choice = input()
+    # print(type(player_choice))
+
+    if player_choice == '1':
+        print('Player Attacks')
+        monster['health'] = monster['health'] - player['attack']
+        print("Monster's Health:")
+        print(monster['health'])
+        
+        print('Monster Attacks')
+        player['health'] = player['health'] - monster['attack']
+        print("Player's Health:")
+        print(player['health'])
+
+    elif player_choice == '2':
+        print('Heal')
+        player['health'] = player['health'] + player['heal']
+        print(player['health'])
+
+    else :
+        print('Invalid input')
     
-    print('Monster Attacks')
-    player['health'] = player['health'] - monster['attack']
-    print("Player's Health:")
-    print(player['health'])
-
-elif player_choice == '2':
-    print('Heal')
-    player['health'] = player['health'] + player['heal']
-    print(player['health'])
+    if player['health'] <= 0 or monster['health'] <= 0:
+        game_running = False
+        

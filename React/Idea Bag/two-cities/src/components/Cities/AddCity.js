014e4620b1ac4553e6da6eed87b67a5cb56d415c
elliottthomlison <elliott.thomlison@gmail.com>
2021-10-25 12:48:23 -0700

    Add a google maps package and update the components further

--- React/Idea Bag/two-cities/src/components/Cities/AddCity.js
+++ React/Idea Bag/two-cities/src/components/Cities/AddCity.js
@@ -1,18 +1,26 @@
 import React from 'react';
 import Button from '../Button/Button';
+import Card from '../UI/Card';
+import classes from './AddCity.module.css'
 
-function AddCity() {
+const AddCity = (props) => {
+  const addCityHandler = (event) => {
+    event.preventDefault();
+    
+  }
+
+
   return (
     // import the card after
-    <div>
-      <form>
+    <Card className={classes.input}>
+      <form onSubmit={addCityHandler}>
         <label htmlFor="fromCity">From:</label>
         <input id="fromcity" type="text" />
         <label htmlFor="toCity">To:</label>
         <input id="toCity" type="text" />
         <Button />
       </form>
-    </div>
+    </Card>
   );
 }
 

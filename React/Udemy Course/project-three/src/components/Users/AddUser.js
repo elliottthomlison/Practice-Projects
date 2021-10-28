c2e9dcc81d04653c6d0b25018d44795e68c9f556
elliottthomlison <elliott.thomlison@gmail.com>
2021-10-27 17:37:12 -0700

    Add data verification checks to the add user function

--- React/Udemy Course/project-three/src/components/Users/AddUser.js
+++ React/Udemy Course/project-three/src/components/Users/AddUser.js
@@ -1,12 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from '../Button/Button';
 import Card from '../UI/Card';
 import classes from './AddUser.module.css'
 
 const AddUser = (props) => {
+    const [enteredUsername, setEnteredUsername] = useState('');
+    const [enteredAge, setEnteredAge] = useState('');
+
     const addUserHandler = (event) => {
-        event.preventDefault();
-        console.log(event.target.value)
+        event.preventDefault(); 
+        // these are the cases we want to prevent (an empty field and an age < 1)
+        if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+           return;
+        } 
+        // the '+' before 'enteredAge' is to allow for the type conversion and comparison between a number and string
+        if (+enteredAge < 1) {
+            return;
+        }
+            console.log(enteredAge, enteredUsername)
+            setEnteredUsername('');
+            setEnteredAge('');
+    }
+
+    const usernameChangeHandler = (event) => {
+        setEnteredUsername(event.target.value);
+    } 
+
+    const ageChangeHandler = (event) => {
+        setEnteredAge(event.target.value);
     }
 
     return (
@@ -14,9 +35,9 @@ const AddUser = (props) => {
             <form onSubmit={addUserHandler}>
                 {/* can use onChange to track the changes */}
                 <label htmlFor="username">Username </label>
-                <input id="username" type="text" />
+                <input id="username" type="text" value={enteredUsername} onChange={usernameChangeHandler}/>
                 <label htmlFor="userage">Age (Years) </label>
-                <input id="userage" type="number" />
+                <input id="userage" type="number" value={enteredAge} onChange={ageChangeHandler}/>
                 <Button type="submit">Add User</Button>
             </form>
         </Card>

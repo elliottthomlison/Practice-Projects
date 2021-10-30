891543d0849d71f125e14119aa1ebdb071a41b48
elliottthomlison <elliott.thomlison@gmail.com>
2021-10-30 09:22:33 -0700

    Add styling to the users and cards

--- React/Udemy Course/project-three/src/components/Users/AddUser.js
+++ React/Udemy Course/project-three/src/components/Users/AddUser.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import Card from '../UI/Card';
+// import './AddUser.css';
+import classes from './AddUser.module.css'
 
 const AddUser = (props) => {
     const addUserHandler = (event) => {
@@ -8,7 +10,7 @@ const AddUser = (props) => {
     }
 
     return (
-        <Card>
+        <Card className={classes.input}>
             <form onSubmit={addUserHandler}>
                 {/* can use onChange to track the changes */}
                 <label htmlFor="username">Username </label>

20f1dcfaed06937b8d75f39d33643870180c3ab0
elliottthomlison <elliott.thomlison@gmail.com>
2021-11-23 12:29:40 -0800

    Upload stylistic changes to the music player c# project

--- C#/MyMusicPlayer/MyMusicPlayer/Form1.Designer.cs
+++ C#/MyMusicPlayer/MyMusicPlayer/Form1.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace MyMusicPlayer
 {
-    partial class Form1
+    partial class MP3
     {
         /// <summary>
         /// Required designer variable.
@@ -29,13 +29,48 @@ namespace MyMusicPlayer
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MP3));
+            this.Player = new AxWMPLib.AxWindowsMediaPlayer();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.Player)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Player
+            // 
+            this.Player.Enabled = true;
+            this.Player.Location = new System.Drawing.Point(0, 0);
+            this.Player.Name = "Player";
+            this.Player.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("Player.OcxState")));
+            this.Player.Size = new System.Drawing.Size(797, 176);
+            this.Player.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(0, 143);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(108, 33);
+            this.textBox1.TabIndex = 1;
+            // 
+            // MP3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Player);
+            this.Name = "MP3";
+            this.Text = "My Music Player";
+            ((System.ComponentModel.ISupportInitialize)(this.Player)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private AxWMPLib.AxWindowsMediaPlayer Player;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- C#/MyMusicPlayer/MyMusicPlayer/Form1.cs
+++ C#/MyMusicPlayer/MyMusicPlayer/Form1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace MyMusicPlayer
 {
-    public partial class Form1 : Form
+    public partial class MP3 : Form
     {
-        public Form1()
+        public MP3()
         {
             InitializeComponent();
         }

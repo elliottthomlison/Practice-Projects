273c9dd3d0eeff63b8987dfac0dffcb11abc32aa
elliottthomlison <elliott.thomlison@gmail.com>
2021-11-29 15:42:07 -0800

    Upload final changes to the HTML and JS

--- JavaScript/CanvasCourse/Part 1/script.js
+++ JavaScript/CanvasCourse/Part 1/script.js
@@ -2,4 +2,6 @@ const canvas = document.querySelector('#canvas');
 const ctx = canvas.getContext('2d');
 
 ctx.fillStyle = "red";
-ctx.fillRect(100, 100, 400, 300)
+ctx.fillRect(100, 100, 500, 300)
+ctx.strokeRect(95, 95, 510, 310)
+ctx.clearRect(150, 150, 400, 200)

2a1d66d7a6f8f8dcaddb5e32c8aba7d8c213d605
elliottthomlison <elliott.thomlison@gmail.com>
2021-12-06 11:01:12 -0800

    Add new folder and file adjustments in React navbar

---
+++ React/navbar-1/src/components/Navbar/MenuItems.js
@@ -0,0 +1,27 @@
+export const MenuItems = [ 
+    {
+        title: 'Home',
+        url:  '#',
+        name: 'nav-links',
+    },
+    {
+        title: 'Services',
+        url:  '#',
+        name: 'nav-links',
+    },
+    {
+        title: 'Products',
+        url:  '#',
+        name: 'nav-links',
+    },
+    {
+        title: 'Contact Us',
+        url:  '#',
+        name: 'nav-links',
+    },
+    {
+        title: 'Sign Up',
+        url:  '#',
+        name: 'nav-links-mobile',
+    },
+]

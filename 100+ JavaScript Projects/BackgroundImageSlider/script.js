a49df58692e4aa364b8ea0a22f383029b834b013
elliottthomlison <elliott.thomlison@gmail.com>
2021-12-17 17:26:47 -0800

    Upload the finished version of the file copied from codepen

--- 100+ JavaScript Projects/BackgroundImageSlider/script.js
+++ 100+ JavaScript Projects/BackgroundImageSlider/script.js
@@ -1,31 +1,53 @@
 window.onload = function () {
-  const buttons = document.querySelectorAll(".btn");
-  const imageDIV = document.querySelector(".img-container");
-  let counter = 0;
+    let sliderImages = document.querySelectorAll(".slide"),
+  arrowLeft = document.querySelector("#arrow-left"),
+  arrowRight = document.querySelector("#arrow-right"),
+  current = 0;
 
-  const images = [
-    "https://static.toiimg.com/photo/msid-72295960/72295960.jpg?545889",
-    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWedyCCbT_HDnqjBoXd17gQka2EoNVonFjnXSTC5ONcwHAdYfZlzH2m6GDsK5jIgV-edw&usqp=CAU",
-    "https://i.guim.co.uk/img/media/26392d05302e02f7bf4eb143bb84c8097d09144b/446_167_3683_2210/master/3683.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=49ed3252c0b2ffb49cf8b508892e452d",
-    "https://static01.nyt.com/images/2021/09/14/science/07CAT-STRIPES/07CAT-STRIPES-mediumSquareAt3X-v2.jpg",
-  ];
+// Clear all images
+function reset() {
+  for (let i = 0; i < sliderImages.length; i++) {
+    sliderImages[i].style.display = "none";
+  }
+}
 
-  buttons.forEach(function (button) {
-    button.onclick = function () {
-      if (button.classList.contains("btn-left")) {
-        counter--;
-        if (counter < 0) {
-          counter = images.length - 1;
-        }
-        imageDIV.style.backgroundImage = `url('./img/${images[counter]}.jpeg')`;
-      }
-      if (button.classList.contains("btn-right")) {
-        counter++;
-        if (counter > images.length - 1) {
-          counter = 0;
-        }
-        imageDIV.style.backgroundImage = `url('./img/${images[counter]}.jpeg')`;
-      }
-    };
-  });
-};
+// Init slider
+function startSlide() {
+  reset();
+  sliderImages[0].style.display = "block";
+}
+
+// Show prev
+function slideLeft() {
+  reset();
+  sliderImages[current - 1].style.display = "block";
+  current--;
+}
+
+// Show next
+function slideRight() {
+  reset();
+  sliderImages[current + 1].style.display = "block";
+  current++;
+}
+
+// Left arrow click
+arrowLeft.addEventListener("click", function() {
+  if (current === 0) {
+    current = sliderImages.length;
+  }
+  slideLeft();
+});
+
+// Right arrow click
+arrowRight.addEventListener("click", function() {
+  if (current === sliderImages.length - 1) {
+    current = -1;
+  }
+  slideRight();
+});
+
+startSlide();
+
+  };
+  

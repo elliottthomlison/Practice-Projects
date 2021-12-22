bfe2861aa3edd2c105f6acfd87c969608facb880
elliottthomlison <elliott.thomlison@gmail.com>
2021-12-22 07:08:56 -0800

    Update the calculator javascript functionality

---
+++ 100+ JavaScript Projects/Calculator/script.js
@@ -0,0 +1,33 @@
+window.onload = function () {
+  const input = document.getElementById('input')
+  const number = document.querySelectorAll('.number')
+  resultDisplayed = false;
+
+// adding click handlers to number buttons
+for (var i = 0; i < number.length; i++) {
+  number[i].addEventListener("click", function(e) {
+
+    // storing current input string and its last character in variables - used later
+    var currentString = input.innerHTML;
+    var lastChar = currentString[currentString.length - 1];
+
+    // if result is not diplayed, just keep adding
+    if (resultDisplayed === false) {
+      input.innerHTML += e.target.innerHTML;
+    } else if (resultDisplayed === true && lastChar === "+" || lastChar === "-" || lastChar === "×" || lastChar === "÷") {
+      // if result is currently displayed and user pressed an operator
+      // we need to keep on adding to the string for next operation
+      resultDisplayed = false;
+      input.innerHTML += e.target.innerHTML;
+    } else {
+      // if result is currently displayed and user pressed a number
+      // we need clear the input string and add the new input to start the new opration
+      resultDisplayed = false;
+      input.innerHTML = "";
+      input.innerHTML += e.target.innerHTML;
+    }
+
+  });
+}
+  
+ };

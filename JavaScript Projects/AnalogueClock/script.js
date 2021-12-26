c378885dcab921977254a55c3eacaba4950d9635
elliottthomlison <elliott.thomlison@gmail.com>
2021-12-26 05:34:28 -0800

    Stopping this project for now to focus on more vanilla JS projects

--- JavaScript Projects/AnalogueClock/script.js
+++ JavaScript Projects/AnalogueClock/script.js
@@ -1,12 +1,26 @@
-const canvas = document.getElementById('canvas');
-const context = canvas.getContext("2d");
-const radius = canvas.height / 2;
-context.translate(radius, radius);
-radius = radius * 0.90
-drawClock();
+window.onload = function () {
+  const canvas = document.getElementById("canvas");
+  const ctx = canvas.getContext("2d");
+  const radius = canvas.height / 2;
+  ctx.translate(radius, radius);
+  radius = radius * 1;
+  drawClock();
 
-function drawClock() {
-  context.arc(0, 0, radius, 0, 2*Math.PI);
-  context.fillStyle = "White";
-  context.fill();
-}
+  function drawClock() {
+    drawFace(ctx, radius)
+  }
+
+  const drawFace = () => {
+    const grad;
+    ctx.beginPath();
+    ctx.arc(0, 0, radius, 0, 2*Math.PI)
+    ctx.fillStyle = 'white'
+    ctx.fill()
+    grad = ctx.createRadialGradient(0, 0, radius*0.95, 0, 0, radius*1.05)
+    grad.addColorStop(0, '#333')
+    
+    ctx.arc(0, 0, radius, 0, 3 * Math.PI);
+    ctx.fillStyle = "white";
+    ctx.fill();
+  }
+};

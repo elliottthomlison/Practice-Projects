d97f1ce0ec9ef5f89a68bd2b6734d94d1b45cad0
elliottthomlison <elliott.thomlison@gmail.com>
2022-01-02 15:13:03 -0800

    Create hooks to toggle the navbar button

--- React/navigation-burger/src/Main.js
+++ React/navigation-burger/src/Main.js
@@ -1,14 +1,29 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./Main.css";
 
 function Main() {
+
+    //create the hooks for the navstatus and the width of the div
+    const [navStatus, setNavStatus] = useState(0)
+    const [divWidth, setDivWidth] = useState('0')
+    
+    function openNav() {
+        if (navStatus == 0) {
+            setDivWidth('100%')
+            setNavStatus(1)
+        } else {
+            setDivWidth('0%')
+            setNavStatus(0)
+        }
+    }
+
     return (
-        <div className="maindiv">
+        <div className="maindiv" style={{width: divWidth}}>
             <h1>Navbar</h1>
 
             <div>
-                <button><i class="fas fa-bars"></i></button>
+                <button onClick={openNav}><i class="fas fa-bars"></i></button>
                 <ul>
                     <li>Home</li>
                     <li>About</li>

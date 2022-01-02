2e4411e409876cd257e5ed94cb9dc36af61ae3b2
elliottthomlison <elliott.thomlison@gmail.com>
2022-01-02 14:57:26 -0800

    Upload the boilerplate code for the nav burger project

---
+++ React/navigation-burger/src/Main.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./Main.css";
+
+function Main() {
+    return (
+        <div>
+            test
+        </div>
+    )
+}
+
+export default Main;

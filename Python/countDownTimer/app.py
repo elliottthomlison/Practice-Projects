8ea2471f1f8f39fd52963c98694577ef8f4d6800
elliottthomlison <elliott.thomlison@gmail.com>
2022-01-03 18:43:55 -0800

    Upload final version of the project

--- Python/countDownTimer/app.py
+++ Python/countDownTimer/app.py
@@ -23,11 +23,11 @@ root.configure(background="black")
 root.bind("x", quit)
 root.after(1000, cant_wait)
 
-endTime = datetime.datetime(2022,2,20)x
+endTime = datetime.datetime(2022,2,20)
 
 words = font.Font(family="Arial", size="120", weight="bold")
 text = StringVar()
-label = ttk.Label(root, textVariable=text, font=words, foreground="white", background="blue")
-label.place(relx=0.5, rely=0.5, anchor=LEFT)
+label = ttk.Label(root, textvariable=text, font=words, foreground="white", background="blue")
+label.place(relx=0.5, rely=0.5, anchor=CENTER)
 
 root.mainloop()

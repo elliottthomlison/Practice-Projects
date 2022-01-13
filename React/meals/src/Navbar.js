e1a76b230325737b7af66b798850a241de00e0e7
elliottthomlison <elliott.thomlison@gmail.com>
2022-01-13 11:42:53 -0800

    Add stylistic changes to the navbar

--- React/meals/src/Navbar.js
+++ React/meals/src/Navbar.js
@@ -2,8 +2,11 @@ import React from 'react';
 
 function Navbar() {
     return (
-        <div>
-            <h1>This is navbar</h1>
+    <div style={{backgroundColor:'brown', color: 'white'}}>
+            <h1>React Meals</h1>
+                <a style={{margin: '10px', color: 'white'}}>Home</a>
+                <a style={{margin: '10px', color: 'white'}}>Order</a>
+                <a style={{margin: '10px', color: 'white'}}>Contact Us</a>
         </div>
     )
 }

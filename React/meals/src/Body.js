9ba5d6fff1c4e40ec2380e3d7b906b6a10c4c162
elliottthomlison <elliott.thomlison@gmail.com>
2022-01-13 11:59:55 -0800

    Add a mapping method to the body.js file to display the data appropriately

--- React/meals/src/Body.js
+++ React/meals/src/Body.js
@@ -1,10 +1,44 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Body() {
+
+    //hook
+    const [items,setItems] = useState([]);
+
+    useEffect(()=>{
+        //the axios call needs a link to the api which it will pull data from
+        axios.get('https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood')
+        //two callback functions
+        .then(response => {
+            //then = success + whatever is inside of the response callback will be executed
+            console.log(response.data.meals)
+
+            setItems(response.data.meals)
+
+        }).catch(error => {
+           //error = failure + whatever is inside of the response callback will be executed
+            console.log(error)
+        })
+    }, [])
+
+    //inside the map method things will be formatted as you call them
+    const itemsList = items.map((object) => {
+        return <div class="col-md-4">
+            <p>{object.strMeal}</p>
+            <img src={object.strMealThumb} className="img-fluid"/>
+            <p>
+                {object.strMeal.idMeal}
+            </p>
+        </div>
+    })
+
     return (
         <div>
-            <h1>This is body</h1>
-
+            <div className='row'>
+                {itemsList}
+            </div>
         </div>
     )
 }

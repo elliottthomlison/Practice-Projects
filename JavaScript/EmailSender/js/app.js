fd7caf16c5be366966f9b07af105d3b78e2fdda9
elliottthomlison <elliott.thomlison@gmail.com>
2022-01-19 15:58:17 -0800

    Update the project with the final code

--- JavaScript/EmailSender/js/app.js
+++ JavaScript/EmailSender/js/app.js
@@ -2,8 +2,9 @@
 const sendBtn = document.getElementById('sendBtn'),
       email = document.getElementById('email'),
       subject = document.getElementById('subject'),
-      message = document.getElementById('message')
-
+      message = document.getElementById('message'),
+      resetBtn = document.getElementById('resetBtn'),
+      sendEmailForm = document.getElementById('email-form')
 
 
 // event listeners
@@ -18,6 +19,10 @@ function eventListeners() {
     email.addEventListener('blur', validateField)
     subject.addEventListener('blur', validateField)
     message.addEventListener('blur', validateField)
+    
+    // send email and reset button
+    sendEmailForm.addEventListener('submit', sendEmail)
+    resetBtn.addEventListener('click', resetForm)
 }
 
 
@@ -27,6 +32,33 @@ function eventListeners() {
 function appInit() {
     // disable button on load
     sendBtn.disabled = true
+}
+
+function sendEmail(e) {
+    e.preventDefault()
+
+    // show the spinner
+    const spinner = document.getElementById('spinner')
+    spinner.style.display = 'block'
+
+    // show the image
+    const sendEmailImg = document.createElement('img')
+    sendEmailImg.src = 'img/mail.gif'
+    sendEmailImg.style.display = 'block'
+
+    // hide the spinner then show email image
+    setTimeout(function() {
+        spinner.style.display = 'none'
+
+        // show the image
+        document.querySelector('#loaders').appendChild(sendEmailImg)
+
+        // after 5 seconds hide
+        setTimeout(function() {
+            sendEmailForm.reset();
+            sendEmailImg.remove()
+        }, 5000)
+    }, 3000)
 }
 
 // validate the fields
@@ -39,6 +71,17 @@ function validateField() {
 
     if (this.type === 'email') {
         validateEmail(this)
+    }
+
+    // both will return errors then check if there're any errors
+    errors = document.querySelectorAll('.error')
+
+    // check that the inputs are validate
+    if (email.value !== '' && subject.value !== '' && message.value !== '') {
+        if (errors.length === 0) {
+            //the button should be enabled
+            sendBtn.disabled = false
+        } 
     }
 }
 
@@ -54,7 +97,6 @@ function validateLength(field) {
 }
 
 // validate the email field and check for the @
-
 function validateEmail(field) {
     let emailText = field.value
 
@@ -66,4 +108,11 @@ function validateEmail(field) {
         field.style.borderBottomColor = 'red'
         field.classList.add('error')
     }
+}
+
+// reset the form
+function resetForm() {
+    sendEmailForm.reset();
+    const mail = document.getElementById('mail')
+    mail.style.display = 'none'
 }
